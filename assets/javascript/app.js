c91388f7b7b62ad3a316c27a9090b496d74e2b6e
Caro Berry <carolbpr@gmail.com>
2019-08-02 13:14:08 -0500

    Game fully working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 /* global firebase */
-
 // Initialize Firebase
 // Make sure that your configuration matches your firebase script version
 // (Ex. 3.0 != 3.7.1)
@@ -22,7 +21,7 @@ var database = firebase.database();
 // All of our connections will be stored in this directory.
 var connectionsRef = database.ref("/connections");
 var resultsRef = database.ref("/results");
-
+var gameRef = database.ref("/game");
 // '.info/connected' is a special location provided by Firebase that is updated
 // every time the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
@@ -32,15 +31,20 @@ var player2 = "";
 var connection = "";
 var playerkey = "not_connected";
 var time;
+var player1wins = 0;
+var player2wins = 0;
+var ties = 0;
 database.ref("/results").set({
     message: "Make your selection to start playing",
     player1wins: 0,
     player2wins: 0,
-    player1: "",
-    player2: ""
-
 });
-displayResults();
+database.ref("/game").set({
+    on: false
+});
+
+console.log()
+
 // When the client's connection state changes...
 connectedRef.on("value", function (snap) {
     console.log(snap);
@@ -72,7 +76,8 @@ connectionsRef.on("value", function (snap) {
     console.log(playerNumber);
     if (playerNumber === 1) {
         clearInterval(time);
-
+        $("#player-1").css("border", "8px solid");
+        $("#player-2").css("border", "3px solid");
         $(".modal").css("display", "block");
         $("#InfoCaption").html("You are Player 1");
         time = setInterval(function () {
@@ -87,14 +92,21 @@ connectionsRef.on("value", function (snap) {
     else if (playerNumber === 2) {
         clearInterval(time);
         $(".modal").css("display", "block");
-        if (last === playerkey)
+        gameRef.on("value", function (snap) {
+            gameOn = snap.val().on
+        });
+        if (last === playerkey && gameOn == false) {
             $("#InfoCaption").html("You are Player 2");
+            $("#player-1").css("border", "3px solid");
+            $("#player-2").css("border", "8px solid");
+        }
         player2 = playerkey;
         console.log(player2);
         time = setInterval(function () {
             $("#InfoCaption").empty(),
                 $("#popupInfo").css("display", "none")
         }, 5000);
+        database.ref("/game/on/").set(true);
     }
     else if (playerNumber > 2) {
         clearInterval(time);
@@ -113,24 +125,6 @@ connectionsRef.on("value", function (snap) {
     console.log("The read failed: " + errorObject.code);
 });
 
-
-
-
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var player1wins = 0;
-var player2wins = 0;
-var ties = 0;
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-//var directionsText = document.getElementById("directions-text");
-//var userChoiceText = document.getElementById("userchoice-text");
-//var computerChoiceText = document.getElementById("computerchoice-text");
-//var winsText = document.getElementById("wins-text");
-//var lossesText = document.getElementById("losses-text");
-//var tiesText = document.getElementById("ties-text");
-
 function resetSelection() {
     // event.preventDefault();
     ref = database.ref("/connections/");
@@ -138,22 +132,55 @@ function resetSelection() {
         snapshot.forEach(function (child) {
             console.log(child);
             database.ref("/connections/" + child.key + "/").set(true);
-            // console.log(child.val());
-            // player1 = child.val();
-            // console.log(player1);
         })
         database.ref("/results/player1/").set("");
         database.ref("/results/player2/").set("");
-
     })
 }
+
 function displayResults() {
     resultsRef.on("value", function (snap) {
-        $("#results").html("<p>" + snap.val().message + "</p>");
-        $("#player-1-score").html("<span>" + snap.val().player1wins + "</span>");
-        $("#player-2-score").html("<span>" + snap.val().player2wins + "</span>");
-        
-        
+        if (snap.val().player1wins >= 5) {
+            message = "Player 1 is the winner!!!";
+            actionMessage = "Make your move to start a New Game";
+            database.ref("/results/message").set(message);
+            database.ref("/results/actionMessage").set(actionMessage);
+            database.ref("/results/player1wins/").set("0");
+            database.ref("/results/player2wins/").set("0");
+            database.ref("/game/on/").set(false);
+            $("#results").html("<p>"+ snap.val().message +"</p>");
+            $("#player-1-score").html("<span>0</span>");
+            $("#player-2-score").html("<span>0</span>");
+            $("#action-message").html("<p>"+snap.val().actionMessage+"</p>")
+            player1wins = 0;
+            player2wins = 0;
+            ties = 0;
+            
+        }
+        else if (snap.val().player2wins >= 5) {
+            message = "Player 2 is the winner!!!";
+            actionMessage = "Make your move to start a New Game";
+            database.ref("/results/message").set(message);
+            database.ref("/results/actionMessage").set(actionMessage);
+            database.ref("/results/player1wins/").set("0");
+            database.ref("/results/player2wins/").set("0");
+            database.ref("/game/on/").set(false);
+            $("#results").html("<p>"+snap.val().message+"</p>");
+            $("#player-1-score").html("<span>0</span>");
+            $("#player-2-score").html("<span>0</span>");
+            $("#action-message").html("<p>"+snap.val().actionMessage+"</p>");
+            player1wins = 0;
+            player2wins = 0;
+            ties = 0;
+            
+        }
+        else{
+            $("#results").html("<p>" + snap.val().message + "</p>");
+            $("#action-message").html("<p>" + snap.val().actionMessage + "</p>");
+            $("#player-1-score").html("<span>" + snap.val().player1wins + "</span>");
+            $("#player-2-score").html("<span>" + snap.val().player2wins + "</span>");
+        }
+
     })
 }
 
@@ -169,93 +196,94 @@ function game() {
 
         if (player1 === "r" && player2 === "s") {
             player1wins++;
+            message = "Rock destroys scissors. Player 1 wins";
+            actionMessage = "Make your move";
             database.ref("/results/player1wins").set(player1wins);
-            message = "Rock destroys sicssors. Player 1 wins";
             database.ref("/results/message").set(message);
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
             resetSelection();
+
         }
         else if (player1 === "s" && player2 === "p") {
             player1wins++;
+            message = "Scissors cut paper. Player 1 win";
+            actionMessage = "Make your move";
             database.ref("/results/player1wins").set(player1wins);
-            message = "Sicssors cut paper. Player 1 win";
             database.ref("/results/message").set(message);
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
             resetSelection();
         }
         else if (player1 === "p" && player2 === "r") {
             player1wins++;
-            database.ref("/results/player1wins").set(player1wins);
             message = "Paper covers rock. Player 1 win";
+            actionMessage = "Make your move";
+            database.ref("/results/player1wins").set(player1wins);
             database.ref("/results/message").set(message);
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
             resetSelection();
 
         }
         else if (player1 === player2) {
             ties++;
             message = "It is a draw";
+            actionMessage = "Make your move";
             database.ref("/results/message").set(message);
+            database.ref("/results/actionMessage").set(actionMessage);
+            resetSelection();
         }
         else if (player1 === "s" && player2 === "r") {
             player2wins++;
+            message = "Rock destroys scissors. Player 2 win";
+            actionMessage = "Make your move";
             database.ref("/results/player2wins").set(player2wins);
-            message = "Rock destroys sicssors. Player 2 win";
             database.ref("/results/message").set(message);
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
             resetSelection();
         }
         else if (player1 === "p" && player2 === "s") {
             player2wins++;
+            message = "Scissors cut paper. Player 2 win";
+            actionMessage = "Make your move";
             database.ref("/results/player2wins").set(player2wins);
-            message = "Sicssors cut paper. Player 2 win";
             database.ref("/results/message").set(message);
-
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
+           
+            
             resetSelection();
         }
         else if (player1 === "r" && player2 === "p") {
             player2wins++;
-            database.ref("/results/player2wins").set(player2wins);
             message = "Paper covers rock. Player 2 win";
+            actionMessage = "Make your move";
+            database.ref("/results/player2wins").set(player2wins);
             database.ref("/results/message").set(message);
-
-            displayResults();
+            database.ref("/results/actionMessage").set(actionMessage);
             resetSelection();
-
         }
-
-        return false;
     }
+    displayResults();
+
 }
 function playerchoose() {
     ref = database.ref("/connections/");
     choose1 = ref.orderByChild("choice").limitToFirst(1).once("value", function (snapshot) {
         snapshot.forEach(function (child) {
             console.log(child.val());
-            if (child.val() == "r"|| child.val() == "p" || child.val() == "s") {
+            if (child.val() == "r" || child.val() == "p" || child.val() == "s") {
                 player1 = child.val();
                 console.log(player1);
                 database.ref("/results/player1/").set(player1);
             }
-
-            //resetSelection();
         });
     });
     choose2 = ref.orderByChild("choice").limitToLast(1).once("value", function (snapshot) {
         snapshot.forEach(function (child) {
             console.log(child.val());
-            if (child.val() == "r"|| child.val() == "p" || child.val() == "s") {
+            if (child.val() == "r" || child.val() == "p" || child.val() == "s") {
                 player2 = child.val();
                 console.log(player2);
                 database.ref("/results/player2").set(player2);
             }
-            //resetSelection();
         });
     });
     game();
@@ -267,23 +295,33 @@ function playerChoicefunction(choice) {
     console.log(choice);
     database.ref("/connections/" + playerkey + "/").set(choice);
     playerchoose();
-}
+
+};
+
+
 
 function choices() {
-    event.preventDefault();
+
     database.ref("/results/message").set("");
-
-
+    database.ref("/results/actionMessage").set("Game On");
     var playerChoice = $(this);
+
     if (playerChoice.attr("data-choice") === "rock") {
+        $("#action-message").html("<p>You selected Rock</p>");
         playerChoicefunction("r");
+
+
     } else if (playerChoice.attr("data-choice") === "paper") {
+        $("#action-message").html("<p>You selected Paper</p>");
         playerChoicefunction("p");
-    } else if (playerChoice.attr("data-choice") === "sicssors") {
+
+    } else if (playerChoice.attr("data-choice") === "scissors") {
+        $("#action-message").html("<p>You selected Scissors</p>");
         playerChoicefunction("s");
 
+
     }
-    console.log(playerkey);
+
 }
 $(document).on("click", ".choice", choices);
 
